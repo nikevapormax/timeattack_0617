a64dbdeb12dec009f2a746652e516943b78e7452
Daegeun Jeong <wjdeorms27@naver.com>
2022-06-17 17:26:30 +0900

    Update : UserType 추가

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import User as UserModel
+from .models import UserType as UserTypeModel
 
 # Register your models here.
 admin.site.register(UserModel)
+admin.site.register(UserTypeModel)
